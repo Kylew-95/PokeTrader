26dc2e385ac1303fd216961b07c01d6851c6f56a
Kyle <kylejordanwilliams@gmail.com>
2023-07-10 19:40:52 +0100

    Updated the Profile page

--- src/Componets/Profile/Profile.js
+++ src/Componets/Profile/Profile.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Profile.css";
 import { supabase } from "../SupabaseLogin/SupabaseLogin";
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import SwiperComp from "./SwiperComp/SwiperComp";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 function Profile({ user, favouriteCard }) {
   const navigate = useNavigate();
@@ -29,41 +30,51 @@ function Profile({ user, favouriteCard }) {
   } else {
     return (
       <>
-        <div className="navExtender"></div>
-        <aside className="ProfileData">
-          <Avatar
-            className="ProfileAvatar"
-            autoCapitalize=""
-            sx={{ width: 170, height: 170, fontSize: "5rem" }}
-          >
-            {user?.user_metadata.full_name[0]}
-          </Avatar>
-          <h2 id="ProfileName">Hi {handleFirstName()}</h2>
-          <p>{user.aud}</p>
-        </aside>
-        <div className="ProfileContainer">
-          <div className="ProfileContainerMain">
-            <h3
-              style={{
-                position: "absolute",
-                display: "flex",
-                marginBottom: "30rem",
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "2rem",
-              }}
-              className="ProfileTitle"
-            >
-              Your Favourite Pokemon
-            </h3>
-            <SwiperComp user={user} favouriteCard={favouriteCard} />
-          </div>
-        </div>
-        <div className="signOutbtn">
-          <Button variant="contained" onClick={handleLogout}>
-            SignOut
-          </Button>
-        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <div className="navExtender"></div>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <aside className="ProfileData">
+              <Avatar
+                className="ProfileAvatar"
+                autoCapitalize=""
+                sx={{ width: 170, height: 170, fontSize: "5rem" }}
+              >
+                {user?.user_metadata.full_name[0]}
+              </Avatar>
+              <h2 id="ProfileName">Hi {handleFirstName()}</h2>
+              <p>{user.aud}</p>
+            </aside>
+            <div className="signOutbtn">
+              <Button variant="contained" onClick={handleLogout}>
+                SignOut
+              </Button>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <div className="ProfileContainer">
+              <div className="ProfileContainerMain">
+                <h3
+                  style={{
+                    position: "absolute",
+                    display: "flex",
+                    marginBottom: "30rem",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "2rem",
+                  }}
+                  className="ProfileTitle"
+                >
+                  Your Favourite Pokemon
+                </h3>
+                <Grid2 sx={{marginLeft: "2vw"}}>
+                  <SwiperComp user={user} favouriteCard={favouriteCard} />
+                </Grid2>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
       </>
     );
   }
