84ef272076ce8b24eea03d2615feaab0d8a76cc6
Kyle <kylejordanwilliams@gmail.com>
2023-07-10 20:45:44 +0100

    Updated the Profile page

--- src/Componets/Profile/Profile.js
+++ src/Componets/Profile/Profile.js
@@ -7,6 +7,7 @@ import SwiperComp from "./SwiperComp/SwiperComp";
 
 function Profile({ user, favouriteCard }) {
   const navigate = useNavigate();
+  console.log("favouriteCard:", favouriteCard[0]?.name);
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
@@ -22,6 +23,17 @@ function Profile({ user, favouriteCard }) {
   function handleFirstName() {
     const firstName = user?.user_metadata.full_name.split(" ")[0];
     return firstName;
+  }
+
+  function stats() {
+    return (
+      <>
+        <div className="statsContainer">
+          <p>{favouriteCard.name}</p>
+          <p>{favouriteCard?.supertype}</p>
+        </div>
+      </>
+    );
   }
 
   if (!user) {
@@ -54,20 +66,18 @@ function Profile({ user, favouriteCard }) {
           <Grid item xs={12} md={8}>
             <div className="ProfileContainer">
               <div className="ProfileContainerMain">
-                <h3
-                  style={{
-                    position: "absolute",
-                    display: "flex",
-                    marginBottom: "30rem",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    fontSize: "2rem",
-                  }}
-                  className="ProfileTitle"
-                >
-                  Your Favourite Pokemon
-                </h3>
-
+                <div className="profileSwiperHeaders">
+                  <h3 style={{ fontSize: "2rem", fontWeight: 600 }}>Stats</h3>
+                  <h3
+                    style={{
+                      fontSize: "2rem",
+                    }}
+                    className="ProfileTitle"
+                  >
+                    Your Favourite Pokemon
+                  </h3>
+                </div>
+                <div className="profileSwiperStats">{stats()}</div>
                 <SwiperComp user={user} favouriteCard={favouriteCard} />
               </div>
             </div>
