73db3511c82a25d9358df4e558c39e5900105f07
Kyle <kylejordanwilliams@gmail.com>
2023-07-13 10:26:09 +0100

    All data can now be retrieved from the backend

--- src/Componets/Profile/SwiperComp/SwiperComp.js
+++ src/Componets/Profile/SwiperComp/SwiperComp.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { supabase } from "../../SupabaseLogin/SupabaseLogin";
-// import miles from "../../Images/pikachu icon.png";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "../SwiperComp/SwiperComp.css";
-
-// import required modules
 import { EffectCoverflow, Pagination } from "swiper/modules";
 import { Button } from "@mui/material";
 
@@ -31,7 +27,6 @@ export default function SwiperComp({ user, favouriteCard }) {
       if (data) {
         setSwiperData(data);
         console.log(data); // this is the data you want to map over
-        // yes, you can map over data here
       }
     }
     fetchUserData();
@@ -48,95 +43,83 @@ export default function SwiperComp({ user, favouriteCard }) {
       }
       console.log("Successfully deleted:", data);
     } catch (error) {
-      console.log("Successfully deleted:", error);
+      console.log("Error deleting:", error);
     }
   }
 
   return (
     <>
-      {/* <div className="profileSwiperHeaders">
-        <h3
-          className="ProfileTitle"
-          style={{ fontSize: "2rem", fontWeight: 600 }}
-        >
-          Stats
-        </h3>
-        <h3
-          style={{
-            fontSize: "2rem",
-          }}
-          className="ProfileTitle"
-        >
-          Your Favourite Pokemon
-        </h3>
-      </div> */}
-      <div className="swiper-Stats">
-        {swiperData.map((item, index) => (
-          <div
-            key={index + 1}
-            style={{ display: index === activeIndex ? "block" : "none" }}
-          >
-            <p>{item.favourite_name}</p>
-            <p>
-              {item.favourite_attacks[0].name}
-              {item.favourite_attacks[0].damage}
-            </p>
-            <br></br>
-            <p style={{ width: "10vw" }}>{item.favourite_attacks[0].text}</p>
+      {swiperData.length === 0 ? (
+        <div className="swiper-Stats">
+          <p>sorry no favourites</p>
+        </div>
+      ) : (
+        <>
+          <div className="swiper-Stats">
+            {swiperData.map((item, index) => (
+              <div
+                key={index + 1}
+                style={{ display: index === activeIndex ? "block" : "none" }}
+              >
+                <h2>{item.favourite_name}</h2>
+                <h3>
+                  {item.favourite_attacks[0].name}
+                  {item.favourite_attacks[0].damage}
+                </h3>
+                <br />
+                <p style={{ width: "10vw" }}>
+                  {item.favourite_attacks[0].text}
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <Swiper
-        initialSlide={2}
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="swiper"
-      >
-        {swiperData.map((photo, index) => (
-          <SwiperSlide className="swiper-slide" key={photo.id}>
-            <div className="swiper-slide">
-              <img src={photo.favourite_cards} alt={photo.favourite_alt} />
-            </div>
-            <Button
-              style={{
-                backgroundColor: "#ffcb05",
-                bottom: "4vh",
-                marginBottom: "0px",
-              }}
-              variant="contained"
-              on
-              onClick={() => {
-                handleDelete(photo.id);
-                const index = swiperData.findIndex(
-                  (item) => item.id === photo.id
-                );
-                setSwiperData((prevData) => {
-                  const newData = [
-                    ...prevData.slice(0, index),
-                    ...prevData.slice(index + 1),
-                  ];
-                  return newData;
-                });
-              }}
-            >
-              Remove
-            </Button>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {/*  */}
+          <Swiper
+            initialSlide={2}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="swiper"
+          >
+            {swiperData.map((photo, index) => (
+              <SwiperSlide className="swiper-slide" key={photo.id}>
+                <div className="swiper-slide">
+                  <img src={photo.favourite_cards} alt={photo.favourite_alt} />
+                </div>
+                <Button
+                  style={{
+                    backgroundColor: "#ffcb05",
+                    bottom: "4vh",
+                    marginBottom: "0px",
+                  }}
+                  variant="contained"
+                  onClick={async () => {
+                    await handleDelete(photo.id);
+                    const index = swiperData.findIndex(
+                      (item) => item.id === photo.id
+                    );
+                    setSwiperData((prevData) =>
+                      prevData.filter((_, i) => i !== index)
+                    );
+                  }}
+                >
+                  Remove
+                </Button>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
     </>
   );
 }
